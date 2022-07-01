f4deaddbaf646cf312040e528f96878ef2d71b52
Woody <gintooooonic@gmail.com>
2022-07-01 15:46:02 +0900

    Delete Swift/woody directory

--- Swift/woody/220623-swift-leetcode/9-palindrome-number.swift
+++
@@ -1,8 +0,0 @@
-class Solution {
-    func isPalindrome(_ x: Int) -> Bool {
-        let s = String(x)
-        let rs = String(s.reversed())
-        let n = s.count / 2 + 1
-        return s.prefix(n) == rs.prefix(n)
-    }
-}
